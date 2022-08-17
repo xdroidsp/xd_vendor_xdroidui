cc1b40ebbf0ada104c92550a7dbc82211d061ef5
xyzuan <xyzuannihboss@gmail.com>
2022-08-17 20:13:08 +0000

    xdroidUI: [REVERT][DNM-TMP] Nuke Pixel Overlay
    This reverts commit 29f4323be307380570ff166340ffd72337bac6c0.

--- configs/xdroidui_overlays.mk
+++ configs/xdroidui_overlays.mk
@@ -29,12 +29,33 @@ endif
 # RRO Overlays
 PRODUCT_PACKAGES += \
     FilesOverlay \
+    PixelLauncherCustomOverlay \
+    PixelDocumentsUIGoogleOverlay \
     BuiltInPrintServiceOverlay \
+    GooglePermissionControllerOverlay \
+    PixelConfigOverlayCommon \
     TeleServiceOverlay \
     CaptivePortalLoginOverlay \
+    GoogleWebViewOverlay \
     TelecomOverlay \
     CellBroadcastServiceOverlay \
+    SettingsGoogleOverlay \
+    SettingsGoogleOverlayPixel2021 \
+    SettingsGoogleProviderOverlay \
     TelephonyProviderOverlay \
     ContactsProviderOverlay \
+    PixelConfigOverlay2018 \
+    SettingsProviderOverlay \
     TraceurOverlay \
+    PixelConfigOverlay2019 \
+    PixelConfigOverlay2019Midyear \
+    PixelConfigOverlay2021 \
+    PixelSetupWizardOverlay \
+    PixelSetupWizardOverlay2019 \
+    PixelSetupWizardOverlayActiveEdge \
+    PixelFwResOverlay \
+    GoogleConfigOverlay \
+    SystemUIGoogleOverlay \
+    CustomPixelLauncherOverlay \
+    XDSetupOverlay \
     NavigationBarModeGesturalOverlayFS
