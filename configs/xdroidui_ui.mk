4f7ccda4c51d68f6bd88904f78b02e065c525150
xyzuan <xyzuannihboss@gmail.com>
2022-10-04 14:52:22 +0000

    xdroidUI: Push Simply Monet Bootanimation

--- configs/xdroidui_ui.mk
+++ configs/xdroidui_ui.mk
@@ -99,10 +99,6 @@ ifeq ($(XDROID_BOOT),)
 $(warning "xdroidUI: Using xd. Dark Bootanimation legacy")
     PRODUCT_COPY_FILES += $(XDUI_PATH)/bootanimation/xd_boot_legacy.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
 else
-ifeq ($(XDROID_BOOT),480)
-$(warning "xdroidUI: Using xd. Dark Bootanimation 480p")
-    PRODUCT_COPY_FILES += $(XDUI_PATH)/bootanimation/xd_boot_480.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
-endif
 ifeq ($(XDROID_BOOT),720)
 $(warning "xdroidUI: Using xd. Dark Bootanimation 720p")
     PRODUCT_COPY_FILES += $(XDUI_PATH)/bootanimation/xd_boot_720.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
